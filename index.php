0d6d49fc09143c73e50e0ea806401283c0a306c3
Alex Watanabe <aowatanabe@gmail.com>
2019-11-16 20:51:48 -0300

    adicionando tarefas para completar o desafio

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+    // TODO:
+    //
+    // OBRIGATÓRIO:
+    // - Cadastro de usuário;
+    // - Login de usuário;
+    // - As postagens precisam apresentar o ​nome do usuário que as criou;​
+    // - Arquivo com o DER deste projeto (diagrama com a estrutura lógica do seu banco de dados - relações entre tabelas, atributos...).
+    // 
+    // OPCIONAL:
+    // — Likes (adicionar botão para ‘dar like’ e também mostrar a quantidade de likes cada publicação teve)
+
+    
     $rotas = key($_GET) ? key($_GET) : "posts";
 
     //direciona para um controlador
