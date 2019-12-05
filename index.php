f0fcde8523c01a21c64aebd6b7fa342034f1fc27
Alex Watanabe <aowatanabe@gmail.com>
2019-12-05 00:08:14 -0300

    finalizado ultimos testes

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php 
 
-    //TODO: deixar volta para a página após o like sem piscar
-
-    //TODO: Arquivo com o DER deste projeto (diagrama com a estrutura lógica do seu banco de dados - relações entre tabelas, atributos...).
-
     //define rota default
     $rotas = key($_GET) ? key($_GET) : "inicio";
 
